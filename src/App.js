55537a3b7072bf88b66ddcff492ef350fff3b2ac
chaudarybilal <chaudarybilal618@gmail.com>
2023-12-12 14:00:29 +0500

    create Navbar

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@
 function App() {
   return (
     <>
-   <div class="bg-slate-900">
-    <nav className="w-full h-14 bg-indigo-200 flex justify-between px-4 md:px-4 ">
-      <div className="text-lg">
+   <div className="bg-slate-900">
+   {/* Navbar start */}
+    <nav className="w-full h-14 bg-indigo-200 flex justify-between items-center px-4 md:px-4 ">
+      <div className="text-2xl text-indigo-700 font-bold">
         PW Skills
       </div>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+      <ul className="md:flex hidden class font-semibold">
+        <li class=" mx-[10px] cursor-pointer">Home</li>
+        <li class="mx-[10px] cursor-pointer">About</li>
+        <li class="mx-[10px] cursor-pointer">Contact</li>
       </ul>
-      <div>
+      <div className="hidden md:block px-2 py-2 bg-indigo-700 text-white rounded font-bold cursor-pointer">
         Login/SignUp
       </div>
+      <div className="md:hidden">
+        <a class="text-4xl" href="" >&#8801;</a>
+      </div>
     </nav>
+    {/* Navbar ending */}
    </div>
    </>
   );
