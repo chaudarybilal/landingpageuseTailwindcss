0dd244087010ea26a1df951bc9d14cc3fbe156e2
chaudarybilal <chaudarybilal618@gmail.com>
2023-12-12 13:34:38 +0500

    create landing page using tailwind css

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+function App() {
+  return (
+    <>
+   <div class="bg-slate-900">
+    <nav className="w-full h-14 bg-indigo-200 flex justify-between px-4 md:px-4 ">
+      <div className="text-lg">
+        PW Skills
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <div>
+        Login/SignUp
+      </div>
+    </nav>
+   </div>
+   </>
+  );
+}
+
+export default App;
